72dbbbf794781bf1c18e65abe9c28578622e6740
GàBạoChúa <phongtom77@gmail.com>
2025-11-15 13:51:58 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from ui.LoginForm import LoginForm 
+
+def main():
+    window = tk.Tk()
+    app = LoginForm(window)
+    window.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+    # ,env dau ?
